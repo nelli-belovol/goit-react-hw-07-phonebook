1eb78f59fb12347e902f629fba83657e5a945342
nelli-belovol <nelli199020@gmail.com>
2021-12-21 11:17:31 +0200

    исправлено название ДЗ

--- src/redux/contacts/contactsSelector.js
+++ src/redux/contacts/contactsSelector.js
@@ -3,3 +3,13 @@ export const getContacts = state => state.contacts.entities;
 export const getFilter = state => state.contacts.filter;
 
 export const isLoading = state => state.contacts.isLoading;
+
+export const getVisibleContacts = state => {
+  const contacts = getContacts(state);
+  const filter = getFilter(state);
+  const normilizedFilter = filter.toLowerCase();
+
+  return contacts.filter(({ contact }) =>
+    contact.name.toLowerCase().includes(normilizedFilter),
+  );
+};
